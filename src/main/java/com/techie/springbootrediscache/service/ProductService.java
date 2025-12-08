a775ed118dc1b84710eb78bc66cd94f341015684
YeganehNobakht <m.yeganeh.n1994@gmail.com>
2025-12-08 13:41:20 +0330

    implementing cache inside services

--- src/main/java/com/techie/springbootrediscache/service/ProductService.java
+++ src/main/java/com/techie/springbootrediscache/service/ProductService.java
@@ -3,27 +3,40 @@ package com.techie.springbootrediscache.service;
 import com.techie.springbootrediscache.dto.ProductDto;
 import com.techie.springbootrediscache.entity.Product;
 import com.techie.springbootrediscache.repository.ProductRepository;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+//    private final CacheManager cacheManager;
 
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
+    @CachePut(value = "PRODUCT_CACHE", key = "#result.id()")
     public ProductDto createProduct(ProductDto productDto) {
         var product = new Product();
         product.setName(productDto.name());
         product.setPrice(productDto.price());
 
         Product savedProduct = productRepository.save(product);
+
+//        we can use this approach or annotation based approach: (@CachePut)
+//        Cache productCache = cacheManager.getCache("PRODUCT_CACHE");
+//        productCache.put(savedProduct.getId(), savedProduct);
+
         return new ProductDto(savedProduct.getId(), savedProduct.getName(),
                 savedProduct.getPrice());
     }
 
+    @Cacheable(value = "PRODUCT_CACHE", key = "#productId")
     public ProductDto getProduct(Long productId) {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new IllegalArgumentException("Cannot find product with id " + productId));
@@ -31,6 +44,7 @@ public class ProductService {
                 product.getPrice());
     }
 
+    @CachePut(value = "PRODUCT_CACHE", key = "#result.id()")
     public ProductDto updateProduct(ProductDto productDto) {
         Long productId = productDto.id();
         Product product = productRepository.findById(productId)
@@ -44,6 +58,7 @@ public class ProductService {
                 updatedProduct.getPrice());
     }
 
+    @CacheEvict(value = "PRODUCT_CACHE", key = "#productId")
     public void deleteProduct(Long productId) {
         productRepository.deleteById(productId);
     }
